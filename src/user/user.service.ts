a3ef428e1860b9b2ee8e3a9895584257860edacd
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-26 03:48:53 +0700

    Add create function

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import {InjectModel} from '@nestjs/mongoose';
+import {User, UserDocument} from './schemas/user.schema';
+import {Model} from 'mongoose';
+import {CreateUserDto} from './dto/createUser.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+}
