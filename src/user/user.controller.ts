1592606fce2741f2d43da62999adb1334d13fbdf
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-26 04:03:30 +0700

    Add route for create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Post} from '@nestjs/common';
+import {CreateUserDto} from './dto/createUser.dto';
+import {UserService} from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  constructor(private userService: UserService) {}
+
+  @Post('create')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+}
