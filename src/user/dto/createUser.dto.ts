431da430ba2fe492e296f920c1e2928252fd4c4a
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-26 03:48:34 +0700

    Create createUser.dto.ts

---
+++ src/user/dto/createUser.dto.ts
@@ -0,0 +1,10 @@
+export class CreateUserDto {
+  readonly username: string;
+  readonly email: string;
+  readonly password: string;
+  readonly url: string;
+  readonly club: null;
+  readonly color: string;
+  readonly emoji: string;
+  readonly reviews: [];
+}
