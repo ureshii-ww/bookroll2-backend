a02640756df7203bd153b1b86c2bc1d579646961
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-26 04:02:42 +0700

    Import Mongoose models to user.module.ts

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import {MongooseModule} from '@nestjs/mongoose';
+import {User, UserSchema} from './schemas/user.schema';
+import {Role, RoleSchema} from './schemas/role.schema';
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController]
+  imports: [MongooseModule.forFeature([
+    { name: User.name, schema: UserSchema },
+    { name: Role.name, schema: RoleSchema }])],
+  controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
