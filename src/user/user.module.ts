32e0a4c2db125747175a6ce4a018f2c479fd3fc7
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-27 02:28:49 +0700

    Import Roles module to User module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,13 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([
-    { name: User.name, schema: UserSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    RolesModule
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService]
