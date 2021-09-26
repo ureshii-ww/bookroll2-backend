97abbdeb5b206e9edd434747c43ea1bb58df862b
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-27 03:23:51 +0700

    Delete garbage

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,10 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { User } from './schemas/user.schema';
 
 @Controller('user')
 export class UserController {
 
   constructor(private userService: UserService) {}
 
-  @ApiOperation({summary: 'Создание пользователя'})
-  @ApiBody({type: CreateUserDto})
-  @ApiResponse({status: 200, type: User})
-  @Post('create')
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
 }
