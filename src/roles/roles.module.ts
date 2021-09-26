3bc766dde8b2bdb059dd276047a1154fe9ce10cb
ureshii-ww <ggggxxxx359@gmail.com>
2021-09-27 02:28:27 +0700

    Add get role by name method

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -6,6 +6,7 @@ import { Role, RoleSchema } from './schemas/role.schema';
 @Module({
   imports: [MongooseModule.forFeature([
     { name: Role.name, schema: RoleSchema }])],
-  providers: [RolesService]
+  providers: [RolesService],
+  exports: [RolesService]
 })
 export class RolesModule {}
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Role, RoleDocument } from './schemas/role.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(@InjectModel(Role.name) private roleModel: Model<RoleDocument>) {}
+
+  async getRoleByName(name: string): Promise<RoleDocument> {
+    return this.roleModel.findOne({name}).exec();
+  }
+}
