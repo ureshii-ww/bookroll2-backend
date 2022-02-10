2706e050d40de575939a06f4dde9e52d1d55f3fb
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-02-10 21:14:07 +0700

    refactor

--- src/club/club.service.ts
+++ src/club/club.service.ts
@@ -1,9 +1,8 @@
 import {
   BadRequestException,
   ConflictException,
-  ForbiddenException,
   Injectable,
-  NotFoundException
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Club, ClubDocument } from './schemas/club.schema';
@@ -14,17 +13,18 @@ import { ClubInfo } from './types/club-info';
 import { CreateClubDto } from './dto/create-club.dto';
 import { AuthUserData } from '../auth/types/authUserData';
 import { ListOfBooksService } from '../list-of-books/list-of-books.service';
-import paginate from '../utils/paginate';
 import { BasicUserInfo } from '../user/types/basic-user-info';
 import { BasicBookInfo } from '../book/types/basic-book-info';
 import { ClubBooks } from './types/club-books';
+import { ListOfBooksDocument } from 'src/list-of-books/schemas/list-of-books.shema';
 
 @Injectable()
 export class ClubService {
-  constructor(@InjectModel(Club.name) private clubModel: Model<ClubDocument>,
-              @InjectModel(User.name) private userModel: Model<UserDocument>,
-              private listOfBooksService: ListOfBooksService) {
-  }
+  constructor(
+    @InjectModel(Club.name) private clubModel: Model<ClubDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private listOfBooksService: ListOfBooksService
+  ) {}
 
   async createClub({ clubname }: CreateClubDto, userUrl: string): Promise<AuthUserData> {
     const user = await this.userModel.findOne({ url: userUrl }).populate('club').exec();
@@ -42,8 +42,8 @@ export class ClubService {
       emoji: userData.emoji,
       roles: userData.roles,
       isEmailConfirmed: userData.isEmailConfirmed,
-      club: newClub.url
-    }
+      club: newClub.url,
+    };
   }
 
   createClubDocument(newClub: Club) {
@@ -64,16 +64,20 @@ export class ClubService {
       master: master._id,
       meetingNumber: 1,
       chosenBooksHistory: null,
-      clubRules: null
-    }
+      clubRules: null,
+    };
   }
 
   async getClubInfo(clubUrl: string, userUrl: string): Promise<ClubInfo> {
-    const clubData = await this.clubModel.findOne({ url: clubUrl })
-      .populate('master').populate('bookToRead').populate('members').exec();
+    const clubData = await this.clubModel
+      .findOne({ url: clubUrl })
+      .populate('master')
+      .populate('bookToRead')
+      .populate('members')
+      .exec();
 
     if (!clubData) {
-      throw new NotFoundException({ message: 'Club not found' })
+      throw new NotFoundException({ message: 'Club not found' });
     }
 
     const isInClub: boolean = clubData.members.some(member => member.url === userUrl);
@@ -81,32 +85,36 @@ export class ClubService {
     const { clubname, master, bookToRead } = clubData;
     return {
       clubname,
-      master: master ? {
-        url: master.url,
-        username: master.username
-      } : null,
-      bookToRead: bookToRead ? {
-        title: bookToRead.title,
-        authors: bookToRead.authors
-      } : null,
+      master: master
+        ? {
+            url: master.url,
+            username: master.username,
+          }
+        : null,
+      bookToRead: bookToRead
+        ? {
+            title: bookToRead.title,
+            authors: bookToRead.authors,
+          }
+        : null,
       isMaster: master ? userUrl === master.url : false,
-      isInClub: isInClub
-    }
+      isInClub: isInClub,
+    };
   }
 
   async joinClub(userUrl: string, clubUrl: string): Promise<AuthUserData> {
     const club = await this.clubModel.findOne({ url: clubUrl }).populate('members').exec();
     if (!club) {
-      throw new BadRequestException({ message: 'Club not found' })
+      throw new BadRequestException({ message: 'Club not found' });
     }
 
     if (club.members.some(member => member.url === userUrl)) {
-      throw new ConflictException({ message: 'User already in the club' })
+      throw new ConflictException({ message: 'User already in the club' });
     }
 
     const user = await this.userModel.findOne({ url: userUrl }).exec();
     if (!user) {
-      throw new BadRequestException({ message: 'User doesn\'t exist' })
+      throw new BadRequestException({ message: "User doesn't exist" });
     }
 
     if (user.club) {
@@ -114,7 +122,7 @@ export class ClubService {
     }
 
     club.members.push(user._id);
-    if(!club.master) {
+    if (!club.master) {
       club.master = user._id;
     }
     await club.save();
@@ -128,8 +136,8 @@ export class ClubService {
       emoji: userData.emoji,
       club: clubUrl,
       roles: userData.roles,
-      isEmailConfirmed: userData.isEmailConfirmed
-    }
+      isEmailConfirmed: userData.isEmailConfirmed,
+    };
   }
 
   async leaveClub(clubUrl: string, userUrl: string): Promise<AuthUserData> {
@@ -171,42 +179,51 @@ export class ClubService {
       emoji: userData.emoji,
       club: null,
       roles: userData.roles,
-      isEmailConfirmed: userData.isEmailConfirmed
-    }
+      isEmailConfirmed: userData.isEmailConfirmed,
+    };
   }
 
   async getClubBooks(url: string, page: number, size: number): Promise<ClubBooks[]> {
-    const club = await this.clubModel.findOne({ url }).populate('club').exec();
+    const club = await this.clubModel.findOne({ url }).populate('members').exec();
     if (!club) {
       throw new BadRequestException();
     }
 
-    const listOfBooks = await this.listOfBooksService.getListOfBooksPopulated(club._id, club.meetingNumber);
-    if (!listOfBooks) {
-      return [];
+    let listOfBooksArray: ListOfBooksDocument[] = [];
+
+    for (let i = 0; i < club.members.length; i++) {
+      const listOfBooks = await this.listOfBooksService.getListOfBooksPopulated(
+        club._id,
+        club.meetingNumber,
+        club.members[i]._id
+      );
+
+      if (listOfBooks) {
+        listOfBooksArray.push(listOfBooks);
+      }
     }
 
-    return listOfBooks.list.map(entry => {
+    return listOfBooksArray.map(entry => {
       const basicUserInfo: BasicUserInfo = {
         username: entry.user.username,
         url: entry.user.url,
         color: entry.user.color,
-        emoji: entry.user.emoji
-      }
+        emoji: entry.user.emoji,
+      };
 
       const cleanedBooks = entry.books.map((book): BasicBookInfo => {
         return {
           id: book._id,
           title: book.title,
           authors: [...book.authors],
-          year: book.year
-        }
+          year: book.year,
+        };
       });
 
       return {
         user: basicUserInfo,
-        books: cleanedBooks
-      }
-    })
+        books: cleanedBooks,
+      };
+    });
   }
 }
