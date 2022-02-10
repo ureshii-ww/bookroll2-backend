8d31e0175ab91f0ec23dadcb020454a6b611da7e
ureshii-ww <63833217+ureshii-ww@users.noreply.github.com>
2022-02-10 20:34:17 +0700

    prettier

--- src/utils/readlyParser.ts
+++ src/utils/readlyParser.ts
@@ -1,15 +1,15 @@
 import * as cheerio from 'cheerio';
 
 //ts-ignores because cheerio is shitty and typed in weird way :))))
-export const readlyParser = (html) => {
+export const readlyParser = html => {
   const $ = cheerio.load(html);
   const title = $('a', '.blvi__title').text();
   const authors = [];
   const genres = [];
   let year = '';
   const description = $('.book--desc').text();
-  const imgTemp = $('a', '.blvi__image').children()[0].attribs.src.split('')
-  imgTemp.splice(-9,2,'2','0');
+  const imgTemp = $('a', '.blvi__image').children()[0].attribs.src.split('');
+  imgTemp.splice(-9, 2, '2', '0');
   const img = 'https://readly.ru' + imgTemp.join('');
   $('*', '.blvi__book_info').each((i, el) => {
     //@ts-ignore
@@ -18,15 +18,17 @@ export const readlyParser = (html) => {
       if (el.attribs.href.includes('genre'))
         //@ts-ignore
         genres.push(el.children[0].data);
-      else
-        //@ts-ignore
-        authors.push(el.children[0].data);
+      //@ts-ignore
+      else authors.push(el.children[0].data);
     }
     if (el.next.type === 'text') {
       //@ts-ignore
       if (el.next.data.match(/[0-9]/)) {
         //@ts-ignore
-        let temp = el.next.data.split('').filter(sym => sym.match(/[0-9]/)).join('');
+        let temp = el.next.data
+          .split('')
+          .filter(sym => sym.match(/[0-9]/))
+          .join('');
         year = temp;
       }
     }
@@ -38,6 +40,6 @@ export const readlyParser = (html) => {
     genres: genres,
     year: year,
     description: description,
-    cover: img
+    cover: img,
   };
-}
+};
